fff83b34054801d68b1a2e9ab122ff3b1d178bf7
Konstantinos Tsakogiannis <75032175+KTsakogiannis@users.noreply.github.com>
2026-02-21 22:50:33 +0200

    Update CSV file name in load_data function

--- bins_2.py
+++ bins_2.py
@@ -59,7 +59,7 @@ if 'route_results' not in st.session_state:
 # --- 2. DATA LOADING ---
 @st.cache_data
 def load_data():
-    return pd.read_csv('ΠΕΡΙΞ_ΕΝΩΠΟΙΗΜΕΝΟ&ΞΑΝΘΗ1_2_3_4_5_6.csv')
+    return pd.read_csv('ΠΕΡΙΞ_ΕΝΩΠΟΙΗΜΕΝΟ&ΞΑΝΘΗ1_2_3_4_5_6_7.csv')
 
 df_full = load_data()
 
